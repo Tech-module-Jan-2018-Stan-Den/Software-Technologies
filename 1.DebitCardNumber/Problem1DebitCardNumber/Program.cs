2658ac3f1ebb2de235eb68a2ec8c3738db189d14
Stanislav <s.k.yordanov@gmail.com>
2018-03-08 17:51:18 +0200

    Team Lead Problem 1 Debit Card C#

---
+++ 1.DebitCardNumber/Problem1DebitCardNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            Console.Write("{0:D4} ", a);
+            Console.Write("{0:D4} ", b);
+            Console.Write("{0:D4} ", c);
+            Console.WriteLine("{0:D4}", d);
+        }
+    }
+}
